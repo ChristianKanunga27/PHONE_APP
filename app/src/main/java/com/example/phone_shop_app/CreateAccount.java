d47e2ea0310b73fdae25373c54d8e25a8762004f
christian <kanungachiristian27@gmail.com>
2026-02-18 01:16:55 +0300

    connection of the nodeSErver to registration

--- app/src/main/java/com/example/phone_shop_app/CreateAccount.java
+++ app/src/main/java/com/example/phone_shop_app/CreateAccount.java
@@ -3,7 +3,10 @@ package com.example.phone_shop_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,12 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONObject;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -31,6 +40,50 @@ public class CreateAccount extends AppCompatActivity {
             Intent intent = new Intent(CreateAccount.this, Login.class);
             startActivity(intent);
         });
+        Button registerBtn = findViewById(R.id.button);
+        EditText edtEmail = findViewById(R.id.edtEmail);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+
+        registerBtn.setOnClickListener(v -> {
+
+            String email = edtEmail.getText().toString();
+            String password = edtPassword.getText().toString();
+
+            new Thread(() -> {
+                try {
+
+                    URL url = new URL("http://192.168.1.164:3000/register");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json");
+                    conn.setDoOutput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("email", email);
+                    jsonParam.put("password", password);
+
+                    OutputStream os = conn.getOutputStream();
+                    os.write(jsonParam.toString().getBytes());
+                    os.flush();
+                    os.close();
+
+                    int responseCode = conn.getResponseCode();
+
+                    runOnUiThread(() -> {
+                        if (responseCode == 200) {
+                            Toast.makeText(this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        });
+
     }
 
     @Override
